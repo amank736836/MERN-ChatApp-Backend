cc409acd7216fbfa38b0813ee1e04f225cb579f1
Aman Kumar <amankarguwal0@gmail.com>
2025-04-09 21:06:04 +0530

    Add database connection utility for MongoDB

---
+++ utils/features.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const connectDB = (url) => {
+  mongoose
+    .connect(url, { dbName: "StealthyNote" })
+    .then((data) => {
+      console.log(`MongoDB connected to: ${data.connection.host}`);
+    })
+    .catch((err) => {
+      throw new Error(err);
+    });
+};
+
+export { connectDB };
