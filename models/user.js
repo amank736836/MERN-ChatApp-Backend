0be0310c217eb666c8852c88a270a53d5394ad9c
Aman Kumar <amankarguwal0@gmail.com>
2025-04-09 21:05:59 +0530

    Add user model with schema for user data management

---
+++ models/user.js
@@ -0,0 +1,46 @@
+import mongoose, { model, Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 3,
+      maxlength: 30,
+    },
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+      minlength: 3,
+      maxlength: 30,
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 6,
+      maxlength: 30,
+      select: false,
+    },
+    avatar: {
+      public_id: {
+        type: String,
+        required: true,
+      },
+      url: {
+        type: String,
+        required: true,
+      },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const userModel = mongoose.models.User || model("User", userSchema, "users");
+
+export default userModel;
