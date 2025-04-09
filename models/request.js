f85c1cd98f17152deb5bda095a21ed77f4cbaaf9
Aman Kumar <amankarguwal0@gmail.com>
2025-04-09 21:05:53 +0530

    Add request model with schema for handling user requests

---
+++ models/request.js
@@ -0,0 +1,29 @@
+import mongoose, { model, Schema } from "mongoose";
+
+const requestSchema = new Schema(
+  {
+    status: {
+      type: String,
+      enum: ["pending", "accepted", "rejected"],
+      default: "pending",
+    },
+    sender: {
+      type: Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    receiver: {
+      type: Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const requestModel =
+  mongoose.models.Request || model("Request", requestSchema, "requests");
+
+export default requestModel;
