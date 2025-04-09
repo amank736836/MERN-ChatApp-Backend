c1fc94b25e17654a46a68ec9e785b48474c965f4
Aman Kumar <amankarguwal0@gmail.com>
2025-04-09 21:05:46 +0530

    Add message model with schema for chat messages and attachments

---
+++ models/message.js
@@ -0,0 +1,39 @@
+import mongoose, { model, Schema, Types } from "mongoose";
+
+const messageSchema = new Schema(
+  {
+    sender: {
+      type: Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    chat: {
+      type: Types.ObjectId,
+      ref: "Chat",
+      required: true,
+    },
+    content: {
+      type: String,
+    },
+    attachments: [
+      {
+        public_id: {
+          type: String,
+          required: true,
+        },
+        url: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const messageModel =
+  mongoose.models.Message || model("Message", messageSchema, "messages");
+
+export default messageModel;
