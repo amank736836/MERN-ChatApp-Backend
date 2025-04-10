ac04eeb7a22ea2c70f5817a329069dcb88dfbfe2
Aman Kumar <amankarguwal0@gmail.com>
2025-04-10 07:25:55 +0530

    Refactor newUser function to validate email and username, and use dynamic name assignment

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,17 +1,41 @@
 import userModel from "../models/user.js";
+import { sendToken } from "../utils/features.js";
 
 const newUser = async (req, res) => {
+  let name = req.body.name;
+
+  const { email, password, username } = req.body;
+
+  if (!email) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Email is required" });
+  }
+
+  if (!username) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Username is required" });
+  }
+
+  if (!name) {
+    name = username;
+  }
+
   const avatar = {
     public_id: "sample_public_id",
     url: "https://example.com/sample_avatar.jpg",
   };
 
-  await userModel.create({
-    name: "John Doe",
-    username: "johndoe",
-    password: "password123",
-    avatar: avatar,
+  const user = await userModel.create({
+    name,
+    email,
+    username,
+    password,
+    avatar,
   });
+
+  sendToken(res, user, 201, "User created successfully");
 
   res.status(201).json({
     success: true,
