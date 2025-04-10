beefbedc3eb1551f377d852ebf722aaabdb1cc6a
Aman Kumar <amankarguwal0@gmail.com>
2025-04-10 07:26:16 +0530

    Add middleware for avatar upload in new user route

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,11 +1,10 @@
 import express from "express";
 import { login, newUser } from "../controllers/user.controller.js";
+import { singleAvatar } from "../middlewares/multer.js";
 
 const user = express.Router();
 
-
-
-user.post("/new", newUser);
+user.post("/new", singleAvatar, newUser);
 user.post("/login", login);
 
 export default user;
