a5a773ef77fc8abdb873bb7b97d676d13f14fd16
Aman Kumar <amankarguwal0@gmail.com>
2025-04-13 17:59:33 +0530

    Refactor user routes to include authentication middleware and improve structure

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,10 +1,43 @@
 import express from "express";
-import { login, newUser } from "../controllers/user.controller.js";
+import jwt from "jsonwebtoken";
+import {
+  getMyProfile,
+  login,
+  logout,
+  newUser,
+  searchUser,
+} from "../controllers/user.controller.js";
+import { ErrorHandler, TryCatch } from "../middlewares/error.js";
 import { singleAvatar } from "../middlewares/multer.js";
 
-const user = express.Router();
+const userRouter = express.Router();
 
-user.post("/new", singleAvatar, newUser);
-user.post("/login", login);
+const isAuthenticated = TryCatch(async (req, res, next) => {
+  const token = req.cookies.StealthyNoteToken;
 
-export default user;
+  if (!token) {
+    return next(new ErrorHandler("Please Login to access this resource", 401));
+  }
+
+  const decodedData = jwt.verify(token, process.env.JWT_SECRET);
+
+  if (!decodedData) {
+    return next(new ErrorHandler("Invalid Token", 401));
+  }
+
+  req.userId = decodedData.id;
+
+  next();
+});
+
+userRouter.post("/new", singleAvatar, newUser);
+userRouter.post("/login", login);
+
+userRouter.use(isAuthenticated);
+
+userRouter.get("/me", getMyProfile);
+userRouter.get("/logout", logout);
+userRouter.get("/search", searchUser);
+
+export default userRouter;
+export { isAuthenticated };
