140d0eae08c3e18e51d6c8dfb0cd73ecaf2b0318
Aman Kumar <amankarguwal0@gmail.com>
2025-04-13 17:56:52 +0530

    Refactor user controller to implement error handling, improve user creation and login logic, and enhance profile retrieval

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,25 +1,23 @@
-import userModel from "../models/user.js";
-import { sendToken } from "../utils/features.js";
+import { compare } from "bcrypt";
+import { ErrorHandler, TryCatch } from "../middlewares/error.js";
+import userModel from "../models/user.models.js";
+import { cookieOptions, sendToken } from "../utils/features.js";
 
-const newUser = async (req, res) => {
+const newUser = TryCatch(async (req, res, next) => {
   let name = req.body.name;
 
   const { email, password, username } = req.body;
 
-  if (!email) {
-    return res
-      .status(400)
-      .json({ success: false, message: "Email is required" });
-  }
+  if (!email) return next(new ErrorHandler("Email is required", 400));
 
-  if (!username) {
-    return res
-      .status(400)
-      .json({ success: false, message: "Username is required" });
-  }
+  if (!email.includes("@")) return next(new ErrorHandler("Invalid email", 400));
+
+  if (!password) return next(new ErrorHandler("Password is required", 400));
+
+  if (!username) return next(new ErrorHandler("Username is required", 400));
 
   if (!name) {
-    name = username;
+    name = username || email.split("@")[0];
   }
 
   const avatar = {
@@ -36,15 +34,64 @@ const newUser = async (req, res) => {
   });
 
   sendToken(res, user, 201, "User created successfully");
+});
 
-  res.status(201).json({
+const login = TryCatch(async (req, res, next) => {
+  const { username, password, email } = req.body;
+
+  if (!username && !email)
+    return next(new ErrorHandler("Username or email is required", 400));
+
+  if (!password) return next(new ErrorHandler("Password is required", 400));
+
+  const user = await userModel
+    .findOne({
+      $or: [{ username }, { email }],
+    })
+    .select("+password");
+
+  if (!user) return next(new ErrorHandler("User not found", 404));
+
+  const isMatch = await compare(password, user.password);
+
+  if (!isMatch) return next(new ErrorHandler("Invalid password", 401));
+
+  sendToken(res, user, 200, "Welcome Back!");
+});
+
+const getMyProfile = TryCatch(async (req, res, next) => {
+  if (!req.userId)
+    return next(new ErrorHandler("Login to access this resource", 401));
+
+  const user = await userModel.findById(req.userId);
+
+  if (!user) return next(new ErrorHandler("User not found", 404));
+
+  res.status(200).json({
     success: true,
-    message: "User created successfully",
+    user,
   });
-};
+});
 
-const login = async (req, res) => {
-  res.send("Login route is working!");
-};
+const logout = TryCatch(async (req, res, next) => {
+  res.cookie("StealthyNoteToken", null, {
+    ...cookieOptions,
+    maxAge: 0,
+  });
 
-export { login, newUser };
+  res.status(200).json({
+    success: true,
+    message: "Logged out successfully",
+  });
+});
+
+const searchUser = TryCatch(async (req, res, next) => {
+  const { name } = req.query;
+
+  res.status(200).json({
+    success: true,
+    message: `Search for ${name}`,
+  });
+});
+
+export { getMyProfile, login, newUser, logout, searchUser };
