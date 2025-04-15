f53ebab2a23400545553bb72e1b4731f4e82770c
Aman Kumar <amankarguwal0@gmail.com>
2025-04-15 07:25:11 +0530

    Add message seeder functions to create messages for chats

---
+++ seeders/message.seeder.js
@@ -0,0 +1,69 @@
+import { faker } from "@faker-js/faker";
+import chatModel from "../models/chat.models.js";
+import messageModel from "../models/message.models.js";
+import userModel from "../models/user.models.js";
+
+const createMessages = async (messagesCount) => {
+  try {
+    const users = await userModel.find().select("_id");
+    const chats = await chatModel.find().select("_id");
+
+    const messagesPromise = [];
+
+    for (let i = 0; i < messagesCount; i++) {
+      const randomChatIndex = Math.floor(Math.random() * chats.length);
+      const randomUserIndex = Math.floor(Math.random() * users.length);
+
+      const randomChat = chats[randomChatIndex];
+      const randomUser = users[randomUserIndex];
+
+      const message = messageModel.create({
+        chat: randomChat,
+        sender: randomUser,
+        content: faker.lorem.sentence(),
+      });
+
+      messagesPromise.push(message);
+    }
+
+    const messages = await Promise.all(messagesPromise);
+
+    console.log(`${messages.length} messages created successfully!`);
+    process.exit(0);
+  } catch (error) {
+    console.error("Error creating messages:", error.message);
+    process.exit(1);
+  }
+};
+
+const createMessageInChat = async (chatId, messagesCount) => {
+  try {
+    const users = await userModel.find().select("_id");
+
+    const messagesPromise = [];
+
+    for (let i = 0; i < messagesCount; i++) {
+      const randomUserIndex = Math.floor(Math.random() * users.length);
+
+      const randomUser = users[randomUserIndex];
+
+      const message = messageModel.create({
+        chat: chatId,
+        sender: randomUser,
+        content: faker.lorem.sentence(),
+      });
+
+      messagesPromise.push(message);
+    }
+
+    const messages = await Promise.all(messagesPromise);
+
+    console.log(`${messages.length} messages created successfully!`);
+    process.exit(0);
+  } catch (error) {
+    console.error("Error creating messages:", error.message);
+    process.exit(1);
+  }
+};
+
+export { createMessageInChat, createMessages };
