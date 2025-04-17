cd86c373dc9a3ae17da3967f996ef2ae4b639824
Aman Kumar <amankarguwal0@gmail.com>
2025-04-17 23:05:39 +0530

    Refactor user routes to use Router from express and add missing route handlers

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,20 +1,26 @@
-import express from "express";
+import express, { Router } from "express";
 import {
+  acceptFriendRequest,
+  getMyFriends,
+  getMyNotifications,
   getMyProfile,
   login,
   logout,
   newUser,
   searchUser,
+  sendFriendRequest,
 } from "../controllers/user.controller.js";
 import { isAuthenticated } from "../middlewares/auth.js";
 import { singleAvatar } from "../middlewares/multer.js";
 import {
+  acceptRequestValidator,
   loginValidator,
   registerValidator,
+  sendRequestValidator,
   validateHandler,
 } from "../utils/validators.js";
 
-const userRouter = express.Router();
+const userRouter = Router();
 
 userRouter.post(
   "/new",
@@ -28,7 +34,24 @@ userRouter.post("/login", loginValidator(), validateHandler, login);
 userRouter.use(isAuthenticated);
 
 userRouter.get("/me", getMyProfile);
+userRouter.get("/notifications", getMyNotifications);
 userRouter.get("/logout", logout);
 userRouter.get("/search", searchUser);
+
+userRouter.put(
+  "/sendRequest",
+  sendRequestValidator(),
+  validateHandler,
+  sendFriendRequest
+);
+
+userRouter.put(
+  "/acceptRequest",
+  acceptRequestValidator(),
+  validateHandler,
+  acceptFriendRequest
+);
+
+userRouter.get("/friends", getMyFriends);
 
 export default userRouter;
