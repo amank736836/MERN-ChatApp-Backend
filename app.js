c29e26476481d017178475010c94f4190c314a1e
Aman Kumar <amankarguwal0@gmail.com>
2025-04-19 20:46:30 +0530

    Refactor environment variable checks for improved clarity and consistency

--- app.js
+++ app.js
@@ -18,13 +18,47 @@ app.use(cookieParser());
 app.use(morgan("dev"));
 
 const mongoDB_uri = process.env.MONGODB_URI;
+const PORT = process.env.PORT || 5000;
+const NODE_ENV = process.env.NODE_ENV.trim() || "production";
+const ADMIN_SECRET_KEY = process.env.ADMIN_SECRET_KEY || "Admin@1234";
+const JWT_SECRET = process.env.JWT_SECRET;
+const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN || "7d";
+
+if (!JWT_SECRET) {
+  console.error("JWT Secret is not defined in the environment variables.");
+  process.exit(1);
+}
+
+if (!ADMIN_SECRET_KEY) {
+  console.error(
+    "Admin Secret Key is not defined in the environment variables."
+  );
+  process.exit(1);
+}
+
+if (!NODE_ENV) {
+  console.error(
+    "Node Environment is not defined in the environment variables."
+  );
+  process.exit(1);
+}
+
+if (!PORT) {
+  console.error("Port is not defined in the environment variables.");
+  process.exit(1);
+}
+
+if (!JWT_EXPIRES_IN) {
+  console.error("JWT Expires In is not defined in the environment variables.");
+  process.exit(1);
+}
+
 if (!mongoDB_uri) {
   console.error("MongoDB URI is not defined in the environment variables.");
   process.exit(1);
 }
+
 connectDB(mongoDB_uri);
-const port = process.env.PORT || 5000;
-const NODE_ENV = process.env.NODE_ENV.trim() || "production";
 
 if (NODE_ENV === "production") {
   app.set("trust proxy", 1);
@@ -49,9 +83,8 @@ app.use("/admin", adminRouter);
 
 app.use(errorMiddleware);
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port} in ${NODE_ENV} mode`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT} in ${NODE_ENV} mode`);
 });
 
-export { NODE_ENV };
-
+export { NODE_ENV, ADMIN_SECRET_KEY, JWT_SECRET, JWT_EXPIRES_IN };
