f8379de102c3f1d5ccf8da40dd3cf85ff985c5f0
Aman Kumar <amankarguwal0@gmail.com>
2025-04-19 19:43:08 +0530

    Refactor error handling middleware to improve error response structure and maintainability

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,3 +1,5 @@
+import { NODE_ENV } from "../app.js";
+
 class ErrorHandler extends Error {
   constructor(message, statusCode) {
     super(message);
@@ -7,20 +9,56 @@ class ErrorHandler extends Error {
 
 const errorMiddleware = (err, req, res, next) => {
   err.message = err.message || "Internal Server Error";
-
   err.statusCode = err.statusCode || 500;
 
-  return res.status(err.statusCode).json({
-    success: false,
+  if (err.code === 11000) {
+    const error = Object.keys(err.keyPattern).join(", ");
+    err.message = `Duplicate value entered for ${error} field, please use another value`;
+    err.statusCode = 400;
+  }
+
+  if (err.name === "CastError") {
+    err.message = `Resource not found. Invalid format: ${err.path}`;
+    err.statusCode = 404;
+  }
+
+  if (err.name === "JsonWebTokenError") {
+    err.message = "Invalid Token, please login again";
+    err.statusCode = 401;
+  }
+
+  if (err.name === "TokenExpiredError") {
+    err.message = "Token expired, please login again";
+    err.statusCode = 401;
+  }
+
+  if (err.name === "ValidationError") {
+    err.message = Object.values(err.errors).map((value) => value.message)[0];
+    err.statusCode = 400;
+  }
+
+  if (err.name === "MulterError") {
+    err.message = err.message || "File upload error";
+    err.statusCode = 400;
+  }
+
+  const response = {
     message: err.message,
-  });
+    statusCode: err.statusCode,
+  };
+
+  if (NODE_ENV === "development") {
+    response.error = err;
+  }
+
+  return res.status(err.statusCode).json(response);
 };
 
 const TryCatch = (fn) => async (req, res, next) => {
   try {
     await fn(req, res, next);
   } catch (error) {
-    next(new ErrorHandler(error.message, error.statusCode));
+    next(error);
   }
 };
 
