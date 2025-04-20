b6f5eb675987e697cd5c65cc9edfe178bca15799
Aman Kumar <amankarguwal0@gmail.com>
2025-04-20 13:21:16 +0530

    Enhance error handling in middleware by adding more specific MongoDB error messages and improving the ErrorHandler constructor defaults

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,16 +1,13 @@
 import { NODE_ENV } from "../app.js";
 
 class ErrorHandler extends Error {
-  constructor(message, statusCode) {
+  constructor(message = "Internal Server Error", statusCode = 500) {
     super(message);
     this.statusCode = statusCode;
   }
 }
 
-const errorMiddleware = (err, req, res, next) => {
-  err.message = err.message || "Internal Server Error";
-  err.statusCode = err.statusCode || 500;
-
+const errCheck = (err) => {
   if (err.code === 11000) {
     const error = Object.keys(err.keyPattern).join(", ");
     err.message = `Duplicate value entered for ${error} field, please use another value`;
@@ -42,6 +39,46 @@ const errorMiddleware = (err, req, res, next) => {
     err.statusCode = 400;
   }
 
+  if (err.code === "ECONNREFUSED") {
+    err.message = "MongoDB connection refused. Please check your connection.";
+    err.statusCode = 500;
+  } else if (err.name === "MongoParseError") {
+    err.message =
+      "MongoDB URI is not valid. Please check your connection string.";
+    err.statusCode = 500;
+  } else if (err.name === "MongoNetworkError") {
+    err.message = "MongoDB network error. Please check your connection.";
+    err.statusCode = 500;
+  } else if (err.name === "MongooseError") {
+    err.message = "Mongoose error. Please check your connection.";
+    err.statusCode = 500;
+  } else if (err.name === "ValidationError") {
+    err.message = "MongoDB validation error. Please check your data.";
+    err.statusCode = 400;
+  } else if (err.name === "MongoTimeoutError") {
+    err.message = "MongoDB connection timeout. Please check your connection.";
+    err.statusCode = 500;
+  } else if (err.name === "MongoServerError") {
+    err.message = "MongoDB server error. Please check your connection.";
+    err.statusCode = 500;
+  } else if (err.name === "MongooseServerSelectionError") {
+    err.message =
+      "MongoDB server selection error. Please check your connection.";
+    err.statusCode = 500;
+  } else if (err.name === "MongooseDisconnectedError") {
+    err.message = "MongoDB disconnected error. Please check your connection.";
+    err.statusCode = 500;
+  } else if (err.name === "MongooseSchemaValidationError") {
+    err.message = "MongoDB schema validation error. Please check your data.";
+    err.statusCode = 400;
+  }
+
+  return err;
+};
+
+const errorMiddleware = (err, req, res, next) => {
+  err = errCheck(err);
+
   const response = {
     message: err.message,
     statusCode: err.statusCode,
@@ -62,4 +99,4 @@ const TryCatch = (fn) => async (req, res, next) => {
   }
 };
 
-export { errorMiddleware, TryCatch, ErrorHandler };
+export { errorMiddleware, TryCatch, ErrorHandler, errCheck };
